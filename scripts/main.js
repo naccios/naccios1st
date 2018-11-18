b91e302a2a53359020c8a7a8ae3752c35e60f763
naccios <naccios@gmail.com>
2018-11-18 19:52:01 -0300

    primer commit
    a ver que sale

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+/*var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+function multiply(num1,num2) {
+    var result = num1 * num2;
+    return result;
+  }
+
+alert(multiply(4, 7)); 
+alert(multiply(20, 20));
+alert(multiply(0.5, 3)); */
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    
+    if(mySrc === 'images/los-dos-pichones.jpg') {
+      myImage.setAttribute ('src','images/helix.jpg');
+    } else {
+      myImage.setAttribute ('src','images/los-dos-pichones.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+
